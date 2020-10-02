d9487a81b13d8f8a69e9f6ecbdaaf280209ace40
Hamamul Fauzi <hamamul@sedaniainnovator.com>
2020-10-02 08:26:01 +0700

    fix todo table name

--- migrations/20200930150908-create-todo.js
+++ migrations/20200930150908-create-todo.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("task_lists", {
+    return queryInterface.createTable("todos", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("task_lists");
+    return queryInterface.dropTable("todos");
   }
 };
